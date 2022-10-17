6f4c6b31d66ab302481d119c29f16a52c5f119f7
Amr <rocket375@outlook.com>
2022-10-17 09:54:25 +0200

    Intiate project models

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from . import models
+
 # Create your views here.
+
+
+class ProjectListView(ListView):
+    model = models.Project
+    template_name = 'project/list.html'
