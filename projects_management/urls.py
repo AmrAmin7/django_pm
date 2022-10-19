8efd3f146524410b33f666d52bb917ff86c7aeae
Amr <rocket375@outlook.com>
2022-10-19 14:22:42 +0200

    Search & Pagination

--- projects_management/urls.py
+++ projects_management/urls.py
@@ -1,4 +1,4 @@
-"""projects_managment URL Configuration
+"""projects_management URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path('', include("projects.urls")),
     path('admin/', admin.site.urls),
 ]
--- projects_management/wsgi.py
+++ projects_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projects_managment project.
+WSGI config for projects_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projects_managment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projects_management.settings')
 
 application = get_wsgi_application()
